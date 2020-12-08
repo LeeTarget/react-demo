adbcfb634801922c46ffca1625c6d0f54728bb1c
LeeChuck <chucken1123@gmail.com>
2020-12-07 22:52:01 -0700

    Remove api key

--- react-video/src/youtube.js
+++ react-video/src/youtube.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {    
-        key: 'AIzaSyAVda0ZcNvFLG0A4t093qLH18HVTrQMkfE',
+        key: '',
         maxResult: 10,
         part: 'snippet',
     }
